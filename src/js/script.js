949143cf77ace310fcf7f12460aa14cfaf6b63c5
Tiago Leandro <tiagoleandro.profissional@gmail.com>
2023-08-06 19:41:26 -0300

    adicionando um svg de onda sobre o form-wrapper com um efeito de transição

--- src/js/script.js
+++ src/js/script.js
@@ -2,13 +2,16 @@ window.addEventListener('click', e => {
 	let stepTwo = document.querySelector('.steps li:nth-child(2)')
 	let stepOne = document.querySelector('.steps li:first-child')
 	let bodyAll = document.querySelector('.all')
+	let formWrapper = document.querySelector('.form-wrapper')
 	if (second.checked){
 		stepTwo.classList.add('step-active')
 		stepOne.classList.add('next-active')
 		bodyAll.classList.add('step-two-active')
+		formWrapper.classList.add('form-wrapper-step-two-active')
 	} else if (!second.checked) {
 		stepTwo.classList.remove('step-active')
 		stepOne.classList.remove('next-active')
 		bodyAll.classList.remove('step-two-active')
+		formWrapper.classList.remove('form-wrapper-step-two-active')
 	}
 })
